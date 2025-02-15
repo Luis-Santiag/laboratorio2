249ce622b4760fcf8ca4d15b0cd06c0d726ded9c
Angel Santiago <lsantiagop1@miumg.edu.gt>
2025-02-15 11:14:41 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
-﻿        Console.Write("Ingresa tu edad: ");
-        int edad = int.Parse(Console.ReadLine());
+﻿string usuarioCorrecto = "Luis";
+string contraseñaCorrecta = "1234";
+
+Console.Write("Ingresa tu nombre de usuario: ");
+string usuario = Console.ReadLine();
+
+Console.Write("Ingresa tu contraseña: ");
+string contraseña = Console.ReadLine();
 
 
-        string categoria;
-        if (edad < 12)
-
-        {categoria = "Niño";}
-        else if (edad >= 12 && edad <= 17)
-
-        {categoria = "Adolescente";}
-
-        else if (edad >= 18 && edad <= 59)
-        {categoria = "Adulto";}
-
-        else
-        {categoria = "Adulto mayor";}
-        Console.WriteLine($"Tu categoría es: {categoria}");
- 
+if (usuario == usuarioCorrecto && contraseña == contraseñaCorrecta)
+{
+    Console.WriteLine("Acceso concedido");
+}
+else if (usuario == usuarioCorrecto && contraseña != contraseñaCorrecta)
+{
+    Console.WriteLine("Contraseña incorrecta");
+}
+else
+{
+    Console.WriteLine("Usuario no registrado");
+}

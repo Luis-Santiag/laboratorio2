d455b56f8d1e0b06249137092258896d342b2e6e
Angel Santiago <lsantiagop1@miumg.edu.gt>
2025-02-15 11:10:13 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿        Console.Write("Ingresa tu edad: ");
+        int edad = int.Parse(Console.ReadLine());
+
+
+        string categoria;
+        if (edad < 12)
+
+        {categoria = "Niño";}
+        else if (edad >= 12 && edad <= 17)
+
+        {categoria = "Adolescente";}
+
+        else if (edad >= 18 && edad <= 59)
+        {categoria = "Adulto";}
+
+        else
+        {categoria = "Adulto mayor";}
+        Console.WriteLine($"Tu categoría es: {categoria}");
+ 
